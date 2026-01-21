291ba3929334eb78d99c27de0d761cc3fbbcb46d
Matarol <91778333+Matarol@users.noreply.github.com>
2026-01-21 10:06:17 +0100

    Basically the initial commit for this new project.

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// A basic route
+app.get('/', (req, res) => {
+  res.send('Hey, there!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
